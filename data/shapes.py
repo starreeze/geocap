2367511989bc8c9be1bf185398f93834c4f8d6dd
Changhao-Xiang <1285157662@qq.com>
2024-08-28 18:58:17 +0800

    update generate_{shape} method

--- data/shapes.py
+++ data/shapes.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union
 from dataclasses import dataclass, asdict
 from abc import ABC, abstractmethod
 from typing import Any
@@ -254,6 +255,11 @@ class Spiral(GSRule):
         return self.initial_radius + (self.growth_rate + epsilon * np.sin(omega * theta + phi)) * theta
 
 
+@dataclass
+class Fusiform(GSRule):
+    pass
+
+
 class ShapeGenerator:
     def __init__(self, rule_args) -> None:
         self.rule_args = rule_args
@@ -267,22 +273,25 @@ class ShapeGenerator:
         self.shape_prob = [x / sum(self.shape_prob) for x in self.shape_prob]
 
     def __call__(self) -> Polygon | Line | Ellipse | Spiral:
+        # generate a shape(GSRule) randomly
         shape_type = np.random.choice(self.opt_shapes, p=self.shape_prob)
         if shape_type == "polygon":
-            shape = self.generate_random_polygon()
+            shape = self.generate_polygon()
         elif shape_type == "line":
-            shape = self.generate_random_line()
+            shape = self.generate_line()
         elif shape_type == "ellipse":
-            shape = self.generate_random_ellipse()
+            shape = self.generate_ellipse()
         elif shape_type == "spiral":
-            shape = self.generate_random_spiral()
+            shape = self.generate_spiral()
         else:
             raise ValueError("invalid type of shape")
         return shape
 
-    def generate_random_polygon(self, max_points=7) -> Polygon:
-        num_points = randint(3, max_points)  # at least 3 points
-        points = [(uniform(0.2, 0.8), uniform(0.2, 0.8)) for _ in range(num_points)]
+    def generate_polygon(self, points:Optional[list[tuple[float,float]]]=None, max_points=6) -> Polygon:
+        if points is None:
+            num_points = randint(3, max_points+1)  # at least 3 points
+            points = [(uniform(0.2, 0.8), uniform(0.2, 0.8)) for _ in range(num_points)]
+        
         polygon = Polygon(points)
         polygon.to_simple_polygon()
         while not polygon.is_convex() or polygon.get_area() < 0.01:
@@ -305,9 +314,11 @@ class ShapeGenerator:
         polygon.normalize_points()
         return polygon
 
-    def generate_random_line(self, min_length=0.2) -> Line:
-        point1 = (uniform(0, 1), uniform(0, 1))
-        point2 = (uniform(0, 1), uniform(0, 1))
+    def generate_line(self, points:Optional[list[tuple[float,float]]]=None, min_length=0.2) -> Line:
+        if points is None:
+            point1 = (uniform(0, 1), uniform(0, 1))
+            point2 = (uniform(0, 1), uniform(0, 1))
+
         line_type = np.random.choice(["line", "segment", "ray"])
         line = Line(type=line_type, points=[point1, point2])
         while line.get_length() < min_length:
@@ -316,23 +327,30 @@ class ShapeGenerator:
             line = Line(type=line_type, points=[point1, point2])
         return line
 
-    def generate_random_ellipse(self) -> Ellipse:
-        center = (uniform(0.2, 0.8), uniform(0.2, 0.8))
-        major_axis = normal(0.5, 0.1)
-        minor_axis = uniform(0.3 * major_axis, major_axis)
-        rotation = uniform(0, np.pi)
-        ellipse = Ellipse(center, major_axis, minor_axis, rotation)
-        while ellipse.get_area() < 0.01:
+    def generate_ellipse(self, center:Optional[tuple[float, float]]= None, major_axis:float=0.0, minor_axis:float=0.0, rotation:float=0.0) -> Ellipse:
+        if center is not None:
+            ellipse = Ellipse(center, major_axis, minor_axis, rotation)
+            if major_axis == minor_axis:
+                ellipse.to_circle(radius=0.5*major_axis)
+            return ellipse
+        
+        else:
+            center = (uniform(0.2, 0.8), uniform(0.2, 0.8))
             major_axis = normal(0.5, 0.1)
             minor_axis = uniform(0.3 * major_axis, major_axis)
+            rotation = uniform(0, np.pi)
             ellipse = Ellipse(center, major_axis, minor_axis, rotation)
+            while ellipse.get_area() < 0.01:
+                major_axis = normal(0.5, 0.1)
+                minor_axis = uniform(0.3 * major_axis, major_axis)
+                ellipse = Ellipse(center, major_axis, minor_axis, rotation)
 
-        special_ellipse = np.random.choice(["no", "circle"])
-        if special_ellipse == "circle":
-            ellipse.to_circle(radius=minor_axis / 2)
-        return ellipse
+            special_ellipse = np.random.choice(["no", "circle"])
+            if special_ellipse == "circle":
+                ellipse.to_circle(radius=minor_axis / 2)
+            return ellipse
 
-    def generate_random_spiral(self) -> Spiral:
+    def generate_spiral(self) -> Spiral:
         center = (uniform(0.2, 0.8), uniform(0.2, 0.8))
         max_theta = uniform(5 * np.pi, 12 * np.pi)
         initial_radius = normal(5e-4, 1e-4)
