a8b3306834d6230ad747402ba435917438cfa014
starreeze <starreeze@foxmail.com>
2024-09-19 10:48:54 +0800

    update eval script & update llava to match latest tf

--- scripts/eval/geo_s1.sh
+++ scripts/eval/geo_s1.sh
@@ -2,8 +2,9 @@ DATADIR=dataset/test/llava
 
 python -m llava.eval.model_vqa_loader \
     --model-path checkpoints/gllava-s2 \
-    --question-file $DATADIR/$1.json \
-    --image-folder $DATADIR/figures \
-    --answers-file $DATADIR/answers/$1.json \
-    --temperature 0 \
+    --question-file $DATADIR/$1.jsonl \
+    --image-folder dataset/test/figures \
+    --answers-file $DATADIR/answers/$1.jsonl \
+    --temperature 0.2 \
+    --max_new_tokens 512 \
     --conv-mode vicuna_v1
