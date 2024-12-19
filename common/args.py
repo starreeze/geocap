c031da4895ead1037cb03f45ba9248355e695900
Dr_osrin <drosrin@qq.com>
2024-12-19 19:20:01 +0800

    fix arg problem

--- common/args.py
+++ common/args.py
@@ -108,15 +108,35 @@ class CaptionArgs:
 
 @dataclass
 class VQAArgs:
+    perspectives: list[str] = field(
+        default_factory=lambda: [
+            "existence",
+            "counting",
+            "size",
+            "location",
+            "reference",
+            "relation",
+        ]
+    )
+    # llm generator
     vqa_batchsize: int = field(default=4)
     vqa_llm: str = field(default="qwen25-7")
     vqa_prompts_dir: str = field(default="data/vqa/prompts")
+    # rule generator
     max_q_ip: int = field(
         default=3,
         metadata={"help": "maximum number of questions per image per perspective"},
     )
     vqa_digits: int = field(default=2, metadata={"help": "number of digits for the answer"})
     nrel_q_prob: float = field(default=0.3, metadata={"help": "probability of no-relation questions"})
+    gt_choice_w: list[float] = field(
+        default_factory=lambda: [0.1, 0.2, 0.3, 0.4],
+        metadata={
+            "help": "weight of the correct answer in the 4 choices; "
+            "we need this to balance the answer distribution "
+            "since smaller values are naturally more likely to be correct"
+        },
+    )
     size_diff: float = field(
         default=0.15,
         metadata={"help": "ratio of the difference of the correct answer and the other choices for size questions"},
@@ -129,6 +149,13 @@ class VQAArgs:
         default=0.1,
         metadata={"help": "tolerate threshold for location difference to be considered"},
     )
+    # evaluation
+    eval_model: str = field(
+        default="llava-7b",
+        metadata={"help": "model name for evaluation. Naming convention: {model_name}-{model_size}"},
+    )
+    eval_batchsize: int = field(default=4)
+    eval_inst: str = field(default="Please directly answer A, B, C or D and nothing else.")
 
 
 data_args, run_args, rule_args, draw_args, caption_args, vqa_args = HfArgumentParser(
