e53a243ee4ae286002e26861997bed3186a0fe67
lxy21214 <112849321+lxy21214@users.noreply.github.com>
2024-12-26 12:27:36 +0800

    Update args.py

--- common/args.py
+++ common/args.py
@@ -162,10 +162,10 @@ class VQAArgs:
     distinguish_threshold_of_relative_direction: float = field(default=0.04)
     deviation_threshold_of_relative_direction: float = field(default=math.pi / 9)
     exclusiv_deviation_threshold_of_relative_direction: float = field(default=math.pi / 5)
-    relative_direction_text_and_vector_dict: dict[str, tuple[float, float]] = field(default_factory=MappingProxyType)
+    relative_direction_text_and_vector_dict: dict[str, tuple[float, float]] = field(default_factory=dict)
     distinguish_threshold_of_absolute_direction: float = field(default=0.1)
     absolute_direction_text_and_box_dict: dict[str, tuple[tuple[float, float], tuple[float, float]]] = (
-        field(default_factory=MappingProxyType)
+        field(default_factory=dict)
     )
     inclusiv_overlapping_threshold_of_absolute_direction: float = field(default=0.8)
 
