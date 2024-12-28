45aa880805930748e3c1bf217122c8e214f0e6ed
starreeze <starreeze@foxmail.com>
2024-12-28 09:47:31 +0800

    update llava scripts

--- data/vqa/question.py
+++ data/vqa/question.py
@@ -6,7 +6,7 @@
 import json
 import os
 
-from common.args import data_args, vqa_args
+from common.args import data_args, logger, run_args, vqa_args
 from data.vqa.llm import LLMQAGenerator
 from data.vqa.rule import RuleBasedQAGenerator
 
@@ -14,8 +14,10 @@ from data.vqa.rule import RuleBasedQAGenerator
 def main():
     os.makedirs(data_args.vqa_question_dir, exist_ok=True)
 
+    if run_args.start_pos:
+        logger.warning(f"starting from {run_args.start_pos}; the questions and figures may be mismatched")
     with open(data_args.rules_path) as f:
-        rules = json.load(f)
+        rules = json.load(f)[run_args.start_pos : run_args.end_pos]
 
     for perspective in vqa_args.perspectives:
         prompt_file = os.path.join(vqa_args.vqa_prompts_dir, f"{perspective}.txt")
