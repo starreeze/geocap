d211faf17ac25e1411f32d12d07c8423e89122da
hyt2002 <42794569+hyt2002@users.noreply.github.com>
2025-01-16 07:56:38 +0800

    Input实现完成，参数调整完成，bug fixed (#65)
    * prompt文本内容移入prompt.py，添加batchsize、llmpath
    
    * 重置gen_input，添加随机给出数值功能
    
    * 修改了rules->prompt，随机丢弃图形数值信息，大模型生成的caption中对于没有数值信息的特殊形状只进行存在性描述
    
    * 位置信息描述修改完成
    
    * 第二阶段部分Caption(WIP)，仍存在部分数据处理的bug
    
    * 删除__pycache__
    
    * 添加对stage参数的支持
    
    * 添加对stage参数的支持，删除pycache
    
    * 修复lint
    
    * 修复lint
    
    * 即使在没有歧义的情况下也会添加至少一层位置信息
    
    * 临时解决代码跑不通问题，gitignore文件中添加忽略：*.pyc
    
    * Input实现完成，参数调整完成，bug fixed

--- data/caption/caption_2nd/shell.py
+++ data/caption/caption_2nd/shell.py
@@ -3,14 +3,15 @@ from data.caption.caption_2nd.base import BaseFeature
 
 
 class Shell(BaseFeature):
-    def __init__(self, type, ratio, length, width, curves, vertices):
+    def __init__(self, type, ratio, length, width, curves, vertices, center):
         self.type = type
         self.ratio = round(ratio, 1)
-        self.length = "{:.1f}".format(round(length))
-        self.width = "{:.1f}".format(round(width))
-        self.length_width_ratio = "{:.1f}".format(round(length / width))
+        self.length = "{:.2f}".format(round(length, 2))
+        self.width = "{:.2f}".format(round(width, 2))
+        self.length_width_ratio = "{:.2f}".format(round(length / width, 2))
         self.curves = curves
         self.vertices = vertices
+        self.center = center
 
     def getShape(self):
         type = self.type
@@ -20,40 +21,79 @@ class Shell(BaseFeature):
             txt = self.standardRangeFilter(fusiform_classes, ratio)
         elif "ellipse" in type:
             txt = self.standardRangeFilter(ellipse_classes, ratio)
+        elif "curves" in type:
+            txt = self.standardRangeFilter(ellipse_classes, ratio)
         equ = self.getEquator()
         if equ != "":
             txt = equ + " " + txt  # type: ignore
         return txt
 
     def getEquator(self):
-        if len(self.curves) != 4:
+        if "curves" in self.type:
+            a1 = self.curves[0][2]
+            top1 = self.vertices[1]
+            b1 = self.curves[1][1]
+            a2 = self.curves[2][2]
+            top2 = self.vertices[3]
+            b2 = self.curves[3][1]
+            angle1 = self.calculate_angle(a1, top1, b1)
+            angle2 = self.calculate_angle(a2, top2, b2)
+            equator1 = self.standardRangeFilter(shell_equator_classes, angle1)
+            equator2 = self.standardRangeFilter(shell_equator_classes, angle2)
+            if equator1 == equator2:
+                return equator1
+            elif equator1 == "inflated":
+                return "top inflated,"
+            elif equator2 == "inflated":
+                return "bottom inflated,"
+        elif "ellipse" in self.type:
+            return self.standardRangeFilter(shell_equator_classes, self.ratio * 20)
+        elif "fusiform" in self.type:
             return ""
-        a1 = self.curves[0][2]
-        top1 = self.vertices[1]
-        b1 = self.curves[1][1]
-        a2 = self.curves[2][2]
-        top2 = self.vertices[3]
-        b2 = self.curves[3][1]
-        angle1 = self.calculate_angle(a1, top1, b1)
-        angle2 = self.calculate_angle(a2, top2, b2)
-        return self.standardRangeFilter(shell_equator_classes, (angle1 + angle2) / 2)
 
     def getSlope(self):
-        # TODO: 侧坡凹凸性
-        return self.standardRangeFilter(shell_slope_classes, 0.4)
+        if "curves" in self.type:
+            return self.standardRangeFilter(shell_pole_classes, 0.4)
+        elif "ellipse" in self.type:
+            return self.standardRangeFilter(shell_slope_classes, 0.4)
+        elif "fusiform" in self.type:
+            return self.standardRangeFilter(shell_slope_classes, 0.6)
 
     def getPole(self):
-        if len(self.curves) != 4:
-            return "bluntly rounded"
-        a1 = self.curves[0][1]
-        top1 = self.vertices[0]
-        b1 = self.curves[3][2]
-        a2 = self.curves[1][2]
-        top2 = self.vertices[2]
-        b2 = self.curves[2][1]
-        angle1 = self.calculate_angle(a1, top1, b1)
-        angle2 = self.calculate_angle(a2, top2, b2)
-        return self.standardRangeFilter(shell_pole_classes, (angle1 + angle2) / 2)
+        if "curves" in self.type:
+            a1 = self.curves[0][1]
+            top1 = self.vertices[0]
+            b1 = self.curves[3][2]
+            a2 = self.curves[1][2]
+            top2 = self.vertices[2]
+            b2 = self.curves[2][1]
+            angle1 = self.calculate_angle(a1, top1, b1)
+            angle2 = self.calculate_angle(a2, top2, b2)
+            return self.standardRangeFilter(shell_pole_classes, (angle1 + angle2) / 2)
+        elif "ellipse" in self.type:
+            return self.standardRangeFilter(shell_pole_classes, 998)
+        elif "fusiform" in self.type:
+            return self.standardRangeFilter(shell_fusiform_pole_classes, self.ratio)
+
+    def getAxis(self):
+        if "curves" in self.type:
+            top = self.center
+            a = self.vertices[0]
+            b = self.vertices[2]
+            cross = self.convex_or_concave(top, a, b)
+            angle = self.calculate_angle(a, top, b)
+            axis = self.standardRangeFilter(shell_axis_classes, angle)
+            if axis == "other":
+                if cross > 0:
+                    return "convex"
+                elif cross < 0:
+                    return "concave"
+            else:
+                return axis
+        elif "ellipse" in self.type:
+            return "straight"
+        elif "fusiform" in self.type:
+            return "straight"
 
     def genUserInput(self):
         txt = "Shell {shape}, ".format(shape=self.getShape())
@@ -61,4 +101,11 @@ class Shell(BaseFeature):
         txt += "the axial length is {length} mm, ".format(length=self.length)
         txt += "and the sagittal width is {width} mm, ".format(width=self.width)
         txt += "width a ratio of length to width of {ratio}. ".format(ratio=self.length_width_ratio)
+        txt += "Axis {convexity}. ".format(convexity=self.getAxis())
+        return txt
+
+    def genInput(self):
+        txt = "length: {length} mm, ".format(length=self.length)
+        txt += "width: {width} mm, ".format(width=self.width)
+        txt += "ratio: {ratio}\n".format(ratio=self.length_width_ratio)
         return txt
