001be4f30fbfbe6d12baf1e1e67005a0a9cd453b
hyt2002 <42794569+hyt2002@users.noreply.github.com>
2025-02-19 18:51:55 +0800

    修复equator描述，添加比例尺，修复initial chamber比例尺 (#80)
    * prompt文本内容移入prompt.py，添加batchsize、llmpath
    
    * 重置gen_input，添加随机给出数值功能
    
    * 修改了rules->prompt，随机丢弃图形数值信息，大模型生成的caption中对于没有数值信息的特殊形状只进行存在性描述
    
    * 位置信息描述修改完成
    
    * 第二阶段部分Caption(WIP)，仍存在部分数据处理的bug
    
    * 删除__pycache__
    
    * 添加对stage参数的支持
    
    * 添加对stage参数的支持，删除pycache
    
    * 修复lint
    
    * 修复lint
    
    * 即使在没有歧义的情况下也会添加至少一层位置信息
    
    * 临时解决代码跑不通问题，gitignore文件中添加忽略：*.pyc
    
    * Input实现完成，参数调整完成，bug fixed
    
    * 一阶段兼容square完成，二阶段壳圈高度与生长速率完成
    
    * 尝试恢复仓库，修复lint
    
    * 二阶段Septa数量、形状/发育程度实现完成
    
    * 修复shell大小，slope、chomata的数值，删去septa counts
    
    * 修复equator描述，添加比例尺
    
    * 修复initial chamber比例尺
    
    * 更改proloculus判断比例尺

--- data/caption/caption_2nd/proloculus.py
+++ data/caption/caption_2nd/proloculus.py
@@ -1,18 +1,22 @@
 from data.caption.caption_2nd.params import *
 from data.caption.caption_2nd.base import BaseFeature
+import math
 
 
 class Proloculus(BaseFeature):
     def __init__(self, type, shape, diameter):
         self.type = type
         self.shape = shape
-        self.diameter = round(diameter, 2)
+        self.diameter0 = diameter
+        self.diameter = round(diameter * shell_world_pixel / shell_pixel_div_mm, 2)
 
     def getShape(self):
         type = self.type
         txt = ""
-        center, weight = self.getCenterAndWeight(self.shape)
-        txt += self.standardRangeFilter(proloculus_size_classes, weight)
+        # center, weight = self.getCenterAndWeight(self.shape)
+        txt += self.standardRangeFilter(
+            proloculus_size_classes, self.diameter0 * shell_world_pixel / shell_pixel_div_mm
+        )
         txt += " "
         # TODO: 肾形
         txt += self.standardRangeFilter(proloculus_shape_classes, 0.1)
@@ -20,7 +24,7 @@ class Proloculus(BaseFeature):
 
     def genUserInput(self):
         txt = "Proloculus {shape}, ".format(shape=self.getShape())
-        txt += "with diameter measuring {diameter}. ".format(diameter=self.diameter)
+        txt += "with diameter measuring {diameter} mm. ".format(diameter=self.diameter)
         return txt
 
     def genInput(self):
