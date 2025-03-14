01895bc014068d4ce245540784d184c102e38888
hyt2002 <605891818@qq.com>
2025-03-14 19:10:54 +0800

    格式化

--- feat_recognize/chomata_scan.py
+++ feat_recognize/chomata_scan.py
@@ -8,6 +8,7 @@ from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 import scipy
 
+
 def chomatas_scan(
     data: dict,
     img_path: str,
@@ -17,7 +18,7 @@ def chomatas_scan(
     default_slice_total_height=20,
     merge_threshold=0.1,
     find_platforms_threshold=0.1,
-    chomata_sensitivity=5.0
+    chomata_sensitivity=5.0,
 ):
     """
     # 旋脊识别chomatas_scan
@@ -73,7 +74,13 @@ def chomatas_scan(
 
         def getOneSlice_down(path: Path, iy):
             step = 1 / line_n
-            slice0 = {"start_points": [], "start_Ts": [], "end_points": [], "pixel_avg": [], "pixel_total": []}
+            slice0 = {
+                "start_points": [],
+                "start_Ts": [],
+                "end_points": [],
+                "pixel_avg": [],
+                "pixel_total": [],
+            }
             for i in range(line_n):
                 T = step * (i + 1)
                 if T < 1 and T > 0:
@@ -86,7 +93,7 @@ def chomatas_scan(
                     mask = np.zeros(img.shape, np.uint8)
                     cv2.line(mask, start_point, end_point, (255, 255, 255), lineType=cv2.LINE_AA)
                     masked_pixels = img[mask > 0]
-                    pixel_total = np.sum(255-masked_pixels)
+                    pixel_total = np.sum(255 - masked_pixels)
                     pixel_average = pixel_total / len(masked_pixels)
                     slice0["start_points"].append(start_point)
                     slice0["start_Ts"].append(T)
@@ -97,7 +104,13 @@ def chomatas_scan(
 
         def getOneSlice_up(path: Path, iy):
             step = 1 / line_n
-            slice0 = {"start_points": [], "start_Ts": [], "end_points": [], "pixel_avg": [], "pixel_total": []}
+            slice0 = {
+                "start_points": [],
+                "start_Ts": [],
+                "end_points": [],
+                "pixel_avg": [],
+                "pixel_total": [],
+            }
             for i in range(line_n):
                 T = step * (i + 1)
                 if T < 1 and T > 0:
@@ -109,7 +122,7 @@ def chomatas_scan(
                     mask = np.zeros(img.shape, np.uint8)
                     cv2.line(mask, start_point, end_point, (255, 255, 255), lineType=cv2.LINE_AA)
                     masked_pixels = img[mask > 0]
-                    pixel_total = np.sum(255-masked_pixels)
+                    pixel_total = np.sum(255 - masked_pixels)
                     pixel_average = pixel_total / len(masked_pixels)
                     slice0["start_points"].append(start_point)
                     slice0["start_Ts"].append(T)
@@ -143,8 +156,9 @@ def chomatas_scan(
         frame_areas = []
         frame_areas_gradient = []
         frame_areas_gradient2 = []
-        img_bg = np.ones((y_step*y_total+1,line_n), np.uint8)*255
-        def bresenham_line(p1,p2):
+        img_bg = np.ones((y_step * y_total + 1, line_n), np.uint8) * 255
+
+        def bresenham_line(p1, p2):
             x0 = p1[0]
             y0 = p1[1]
             x1 = p2[0]
@@ -169,6 +183,7 @@ def chomatas_scan(
                     y0 += sy
 
             return points
+
         for y in range(y_total):
             y_idx = y + 1
             slice0 = getOneSlice(path, y_idx)
@@ -177,14 +192,14 @@ def chomatas_scan(
             frame_area = []
             xs = list(range(len(frame2)))
             for end_frame_pos in range(len(frame2)):
-                frame_area.append(np.trapz(frame2[0:end_frame_pos+1]))
-            frame_areas_gradient.append(scipy.signal.savgol_filter(frame2,15,3))
-            frame_area_g2 = np.gradient(frame2,xs)
-            frame_area_g2=scipy.signal.savgol_filter(frame_area_g2,15,3)
+                frame_area.append(np.trapz(frame2[0 : end_frame_pos + 1]))
+            frame_areas_gradient.append(scipy.signal.savgol_filter(frame2, 15, 3))
+            frame_area_g2 = np.gradient(frame2, xs)
+            frame_area_g2 = scipy.signal.savgol_filter(frame_area_g2, 15, 3)
             # frame_area = np.gradient(frame_area,xs)
             frame_areas.append(frame_area)
             frame_areas_gradient2.append(frame_area_g2)
-            if y == y_total-1:
+            if y == y_total - 1:
                 for iii in range(len(slice0["start_points"])):
                     # st = slice0["start_points"][iii]
                     # ed = slice0["end_points"][iii]
@@ -195,11 +210,11 @@ def chomatas_scan(
                     # M = cv2.getAffineTransform(np.array([st,mi,ed],dtype=np.float32),np.array([dst_st,dst_mi,dst_ed],dtype=np.float32))
                     # cutted_original = cv2.bitwise_and(img,img,mask=slice0["masked_pixels"][iii])
                     # dst = cv2.warpAffine(cutted_original,M,(img_bg.shape[1],img_bg.shape[0]))
-                    ps = bresenham_line(slice0["start_points"][iii],slice0["end_points"][iii])
+                    ps = bresenham_line(slice0["start_points"][iii], slice0["end_points"][iii])
                     for yyy in range(img_bg.shape[0]):
-                        if yyy<len(ps):
-                            if ps[yyy][1]<len(img) and ps[yyy][0]<len(img[ps[yyy][1]]):
-                                img_bg[yyy][iii]=img[ps[yyy][1]][ps[yyy][0]]
+                        if yyy < len(ps):
+                            if ps[yyy][1] < len(img) and ps[yyy][0] < len(img[ps[yyy][1]]):
+                                img_bg[yyy][iii] = img[ps[yyy][1]][ps[yyy][0]]
             peak_height = getMinPeakHeight()
             peaks, _ = find_peaks(frame, distance=distance, width=peak_width, height=peak_height)
             peaks = list(peaks)
@@ -216,161 +231,179 @@ def chomatas_scan(
             key=lambda x: (255 - sum(x["pixel_avg"]) / len(x["pixel_avg"])) + len(x["Ts"]) * 1000,
             reverse=True,
         )
+
         def standardization(unstd_data):
             # mu = np.mean(unstd_data, axis=0)
             mu = 0
             sigma = np.max(abs(unstd_data))
             return (unstd_data - mu) / sigma
+
         def normalization(unnorm_data):
             _range = np.max(unnorm_data) - np.min(unnorm_data)
             return (unnorm_data - np.min(unnorm_data)) / _range
+
         def my_sigmoid_modified(s_x):
-            return 1/(1+math.exp(-10*(s_x-0.5)))
-        def find_platforms(grad2_,areas):
-            grad2_neg=np.zeros(len(grad2_),dtype=np.float64)
-            grad2_neg[grad2_<0]=grad2_[grad2_<0]
+            return 1 / (1 + math.exp(-10 * (s_x - 0.5)))
+
+        def find_platforms(grad2_, areas):
+            grad2_neg = np.zeros(len(grad2_), dtype=np.float64)
+            grad2_neg[grad2_ < 0] = grad2_[grad2_ < 0]
             grad2_neg = abs(grad2_neg)
-            ed_peak,un_pe = find_peaks(grad2_neg,height=find_platforms_threshold)
-            grad2_pos=np.zeros(len(grad2_),dtype=np.float64)
-            grad2_pos[grad2_>0]=grad2_[grad2_>0]
-            st_peak,un_pe = find_peaks(grad2_pos,height=find_platforms_threshold)
-            ist=0
-            ied=0
+            ed_peak, un_pe = find_peaks(grad2_neg, height=find_platforms_threshold)
+            grad2_pos = np.zeros(len(grad2_), dtype=np.float64)
+            grad2_pos[grad2_ > 0] = grad2_[grad2_ > 0]
+            st_peak, un_pe = find_peaks(grad2_pos, height=find_platforms_threshold)
+            ist = 0
+            ied = 0
             p_pairs = []
-            while ist<len(st_peak) or ied<len(ed_peak):
-                min_p=None
-                if ist<len(st_peak):
-                    min_p = (st_peak[ist],1)
-                if ied<len(ed_peak):
-                    if min_p is None or min_p[0]>ed_peak[ied]:
-                        min_p = (ed_peak[ied],-1)
-                if min_p[1]<0:
-                    ied+=1
+            while ist < len(st_peak) or ied < len(ed_peak):
+                min_p = None
+                if ist < len(st_peak):
+                    min_p = (st_peak[ist], 1)
+                if ied < len(ed_peak):
+                    if min_p is None or min_p[0] > ed_peak[ied]:
+                        min_p = (ed_peak[ied], -1)
+                if min_p[1] < 0:
+                    ied += 1
                 else:
-                    ist+=1
+                    ist += 1
                 p_pairs.append(min_p)
-            if len(p_pairs)==1:
-                if p_pairs[0][1]<0:
-                    p_pairs=[(0,1)]+p_pairs
+            if len(p_pairs) == 1:
+                if p_pairs[0][1] < 0:
+                    p_pairs = [(0, 1)] + p_pairs
                 else:
-                    p_pairs.append((len(grad2_)-1,-1))
+                    p_pairs.append((len(grad2_) - 1, -1))
             else:
-                if p_pairs[0][1]<0:
-                    p_pairs=[(0,1)]+p_pairs
-                if p_pairs[-1][1]>0:
-                    p_pairs.append((len(grad2_)-1,-1))
+                if p_pairs[0][1] < 0:
+                    p_pairs = [(0, 1)] + p_pairs
+                if p_pairs[-1][1] > 0:
+                    p_pairs.append((len(grad2_) - 1, -1))
             p_pairs_merged = []
             temp = None
             for ip in range(len(p_pairs)):
                 if temp is None:
                     temp = p_pairs[ip]
                     continue
-                if p_pairs[ip][1]==temp[1]:
-                    if abs(grad2_[p_pairs[ip][0]])>abs(grad2_[temp[0]]):
+                if p_pairs[ip][1] == temp[1]:
+                    if abs(grad2_[p_pairs[ip][0]]) > abs(grad2_[temp[0]]):
                         temp = p_pairs[ip]
                     continue
                 else:
                     p_pairs_merged.append(temp)
                     temp = p_pairs[ip]
             p_pairs_merged.append(temp)
-            ip=0
+            ip = 0
             p_pairs_final_merged = []
             temp = []
+
             def cal_area(idx0):
                 total_area = 0
                 for area in areas:
-                    total_area+=(normalization(area))[idx0]
-                return total_area/len(areas)
-            while ip<len(p_pairs_merged):
+                    total_area += (normalization(area))[idx0]
+                return total_area / len(areas)
+
+            while ip < len(p_pairs_merged):
                 if temp == []:
-                    temp = [p_pairs_merged[ip],p_pairs_merged[ip+1]]
+                    temp = [p_pairs_merged[ip], p_pairs_merged[ip + 1]]
                 else:
-                    if abs(cal_area(p_pairs_merged[ip][0])-cal_area(temp[1][0]))<merge_threshold:
-                        temp[1] = p_pairs_merged[ip+1]
-                        if grad2_[p_pairs_merged[ip][0]]>grad2_[temp[0][0]]:
-                            temp[0]=p_pairs_merged[ip]
+                    if abs(cal_area(p_pairs_merged[ip][0]) - cal_area(temp[1][0])) < merge_threshold:
+                        temp[1] = p_pairs_merged[ip + 1]
+                        if grad2_[p_pairs_merged[ip][0]] > grad2_[temp[0][0]]:
+                            temp[0] = p_pairs_merged[ip]
                     else:
                         p_pairs_final_merged.extend(temp)
-                        temp = [p_pairs_merged[ip],p_pairs_merged[ip+1]]
-                ip+=2
+                        temp = [p_pairs_merged[ip], p_pairs_merged[ip + 1]]
+                ip += 2
             p_pairs_final_merged.extend(temp)
             return p_pairs_merged
-        peaks_plat = find_platforms(standardization(sum(frame_areas_gradient2)/len(frame_areas_gradient2)),frame_areas)
+
+        peaks_plat = find_platforms(
+            standardization(sum(frame_areas_gradient2) / len(frame_areas_gradient2)), frame_areas
+        )
         plt.figure(dpi=100)
-        plt.imshow(img_bg,origin="lower")
-        std_grad2 = standardization(sum(frame_areas_gradient2)/len(frame_areas_gradient2))*y_step*y_total
-        plt.plot(list(range(len(frame_areas_gradient2[0]))),std_grad2)
-        plt.plot(list(range(len(frame_areas_gradient[0]))),standardization(sum(frame_areas_gradient)/len(frame_areas_gradient))*y_step*y_total)
+        plt.imshow(img_bg, origin="lower")
+        std_grad2 = (
+            standardization(sum(frame_areas_gradient2) / len(frame_areas_gradient2)) * y_step * y_total
+        )
+        plt.plot(list(range(len(frame_areas_gradient2[0]))), std_grad2)
+        plt.plot(
+            list(range(len(frame_areas_gradient[0]))),
+            standardization(sum(frame_areas_gradient) / len(frame_areas_gradient)) * y_step * y_total,
+        )
         for p in peaks_plat:
-            plt.plot(p[0],std_grad2[p[0]],marker="o",color="red" if p[1]>0 else "green")
+            plt.plot(p[0], std_grad2[p[0]], marker="o", color="red" if p[1] > 0 else "green")
         for a in frame_areas:
-            plt.plot(list(range(len(a))),normalization(a)*y_step*y_total)
+            plt.plot(list(range(len(a))), normalization(a) * y_step * y_total)
             # plt.plot(list(range(len(a))),a)
-        ax=plt.gca()
+        ax = plt.gca()
         ratio = 0.3
-        x_left, x_right = ax. get_xlim ()
-        y_low, y_high = ax. get_ylim ()
-        ax. set_aspect ( abs ((x_right-x_left)/(y_low-y_high))*ratio)
+        x_left, x_right = ax.get_xlim()
+        y_low, y_high = ax.get_ylim()
+        ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * ratio)
         # for bin in peak_bins:
         #     bin["chosen_point"] = points_max(bin["end_points"], bin["pixel_avg"])
         ip = 0
         final_chomatas = []
         final_chomatas_debug = []
+
         def get_all_candidates_width_norm():
             widths = []
-            ip___=0
-            while ip___<len(peaks_plat):
-                widths.append(peaks_plat[ip___+1][0]-peaks_plat[ip___][0])
+            ip___ = 0
+            while ip___ < len(peaks_plat):
+                widths.append(peaks_plat[ip___ + 1][0] - peaks_plat[ip___][0])
                 ip___ += 2
-            return standardization(np.array(widths,dtype=np.float64))
+            return standardization(np.array(widths, dtype=np.float64))
+
         all_candidates_norm = get_all_candidates_width_norm()
-        def get_confidence(p_st,p_ed,ip__,p_ed_before=None):
-            avg_grad1 = [] # 平均一阶导
-            avg_y = [] # 平均面积序列
+
+        def get_confidence(p_st, p_ed, ip__, p_ed_before=None):
+            avg_grad1 = []  # 平均一阶导
+            avg_y = []  # 平均面积序列
             avg_grad_before = []
             for a in frame_areas:
-                aa=normalization(a)*line_n
-                avg_grad1.append((aa[p_ed[0]]-aa[p_st[0]])/(p_ed[0]-p_st[0]))
-                avg_y.append(float(aa[p_ed[0]]-aa[p_st[0]]))
+                aa = normalization(a) * line_n
+                avg_grad1.append((aa[p_ed[0]] - aa[p_st[0]]) / (p_ed[0] - p_st[0]))
+                avg_y.append(float(aa[p_ed[0]] - aa[p_st[0]]))
                 # 计算chomata存在性（如果有）
                 if p_ed_before is not None:
-                    avg_grad_before.append((aa[p_st[0]]-aa[p_ed_before[0]])/(p_st[0]-p_ed_before[0]))
+                    avg_grad_before.append((aa[p_st[0]] - aa[p_ed_before[0]]) / (p_st[0] - p_ed_before[0]))
             conf = float(np.average(avg_y))
             # conf = conf*0.9+all_candidates_norm[ip__//2]*0.1
             if p_ed_before is not None:
                 total_conf_ab = 0
-                for a,b in zip(avg_grad1,avg_grad_before):
-                    total_conf_ab += a/b
+                for a, b in zip(avg_grad1, avg_grad_before):
+                    total_conf_ab += a / b
             return conf
-        while ip<len(peaks_plat):
+
+        while ip < len(peaks_plat):
             if ip == 0:
                 p_before = None
-                if peaks_plat[ip][0]>0:
-                    p_before = (0,1)
-                c0 = get_confidence(peaks_plat[ip],peaks_plat[ip+1],ip,p_before)
+                if peaks_plat[ip][0] > 0:
+                    p_before = (0, 1)
+                c0 = get_confidence(peaks_plat[ip], peaks_plat[ip + 1], ip, p_before)
                 if c0 > chomata_sensitivity:
-                    x0 = (peaks_plat[ip][0]+peaks_plat[ip+1][0])/2
-                    T0 = x0/line_n
+                    x0 = (peaks_plat[ip][0] + peaks_plat[ip + 1][0]) / 2
+                    T0 = x0 / line_n
                     p0 = path.point(T0)
-                    final_chomatas_debug.append((int(x0),c0))
-                    final_chomatas.append((math.floor(p0.real),math.floor(p0.imag),c0))
+                    final_chomatas_debug.append((int(x0), c0))
+                    final_chomatas.append((math.floor(p0.real), math.floor(p0.imag), c0))
             else:
-                p_before = peaks_plat[ip-1]
-                c0 = get_confidence(peaks_plat[ip],peaks_plat[ip+1],ip,p_before)
+                p_before = peaks_plat[ip - 1]
+                c0 = get_confidence(peaks_plat[ip], peaks_plat[ip + 1], ip, p_before)
                 if c0 > chomata_sensitivity:
-                    x0 = (peaks_plat[ip][0]+peaks_plat[ip+1][0])/2
-                    T0 = x0/line_n
+                    x0 = (peaks_plat[ip][0] + peaks_plat[ip + 1][0]) / 2
+                    T0 = x0 / line_n
                     p0 = path.point(T0)
-                    final_chomatas_debug.append((int(x0),c0))
-                    final_chomatas.append((math.floor(p0.real),math.floor(p0.imag),c0))
+                    final_chomatas_debug.append((int(x0), c0))
+                    final_chomatas.append((math.floor(p0.real), math.floor(p0.imag), c0))
             ip += 2
         # return [bin["chosen_point"] for bin in peak_bins]  # type: ignore
         for p in final_chomatas_debug:
-            plt.plot(p[0],std_grad2[p[0]],marker="x",color="blue")
-            plt.text(p[0],std_grad2[p[0]],str(round(p[1],4)))
+            plt.plot(p[0], std_grad2[p[0]], marker="x", color="blue")
+            plt.text(p[0], std_grad2[p[0]], str(round(p[1], 4)))
         plt.show()
-        final_chomatas.sort(key=lambda x:x[2],reverse=True)
-        return final_chomatas if len(final_chomatas)>2 else final_chomatas
+        final_chomatas.sort(key=lambda x: x[2], reverse=True)
+        return final_chomatas if len(final_chomatas) > 2 else final_chomatas
 
     def parsePathUsingLines(points):
         if type(points) == Path:
@@ -405,12 +438,12 @@ def chomatas_scan(
                     p2 = path2.point(ans[0][0])
                     length = Line(p, p2).length()
                     total_height += length
-                    total_intersects+=1
+                    total_intersects += 1
                     if length < min_height:
                         min_height = length
         if has_intersects:
             # return int(min_height - 2)
-            return int(total_height/total_intersects*0.8)
+            return int(total_height / total_intersects * 0.8)
         else:
             return default_slice_total_height
 
